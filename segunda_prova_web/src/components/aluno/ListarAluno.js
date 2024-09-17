8ad426b4174a9a2945acc3430557f2a5242fe47a
João Paulo N. Rodrigues <joaopaulonr.cloud@gmail.com>
2024-09-16 21:02:30 -0300

    alunos add

---
+++ segunda_prova_web/src/components/aluno/ListarAluno.js
@@ -0,0 +1,85 @@
+import { Link } from "react-router-dom";
+
+import "../../css/crud.css";
+import AlunoService from "../../services/AlunoService";
+
+import { useState, useEffect } from "react";
+
+
+const ListarAluno = () => {
+  const [alunos, setAlunos] = useState([]);
+
+  useEffect(() => {
+    AlunoService.getAlunosAxiosAsyncAwait((json) => {
+      setAlunos(json);
+    });
+  }, []);
+
+  const deleteAluno = (id) => {
+    if(window.confirm(`Deseja realmente excluir id = ${id}`)){
+      AlunoService.deleteAlunoById(
+        id,
+        (response) => {
+          //console.log(response)
+          const res = alunos.filter(
+            (aluno) => aluno._id !== id
+          )
+          //console.log(res)
+          setAlunos(res)
+        }
+      )
+    }
+  }
+
+  const corpoTabela = () => {
+    const novoArray = alunos.map(
+      (aluno) => {
+        return (
+          <tr>
+            <th scope="row">{aluno._id}</th>
+            <td>{aluno.nome}</td>
+            <td>{aluno.curso}</td>
+            <td>{aluno.titulacao}</td>
+            <td className="button-content">
+
+              <Link
+                className="btn btn-primary"
+                to={`/alunos/editar/${aluno._id}`}
+              >
+                Editar
+              </Link>
+
+              <button 
+                className="btn btn-danger"
+                onClick={() => deleteAluno(aluno._id)}
+              >
+                Apagar
+              </button>
+            </td>
+          </tr>
+        ); //return de cada elemento como um JSX
+      } //funcao arrow
+    ); //map
+    return novoArray;
+  };
+
+  return (
+    <div className="page-content">
+      <h1>Listar Alunos</h1>
+      <table className="table table-striped table-content table-bordered">
+        <thead className="table-dark">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Curso</th>
+            <th scope="col">Titulação</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>{corpoTabela()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListarAluno;
